88b374ede3d832e20922c4986895974625ad5d26
DennyAge <DenX055@gmail.com>
2025-01-31 01:12:31 +0200

    minor changes

--- components/SortBtn.tsx
+++ components/SortBtn.tsx
@@ -11,7 +11,7 @@ const SortBtn = ({ isActive, onClick, color = "bg-default-600" }: Props) => {
       aria-label={isActive ? "Sort active" : "Sort inactive"}
       className="flex flex-col justify-center items-center cursor-pointer mt-1"
     >
-      <div className="flex flex-col gap-[3px] w-[10px] h-[10px] md:w-[12px] md:h-[12px]">
+      <div className="flex flex-col gap-[2px]  sm:gap-[3px] w-[10px] h-[10px] md:w-[12px] md:h-[12px]">
         <span
           className={`h-[1px] ${color} transition-all duration-300 ease-in-out ${isActive ? "w-full" : "w-[50%]"}`}
         ></span>
