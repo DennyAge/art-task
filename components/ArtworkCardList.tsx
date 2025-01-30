f574a91397fe2b31423707e90566ac46a686a6be
DennyAge <DenX055@gmail.com>
2025-01-30 21:48:22 +0200

    fix mobile view

--- components/ArtworkCardList.tsx
+++ components/ArtworkCardList.tsx
@@ -21,7 +21,7 @@ const ArtworkCardList = ({ artworks }: Props) => {
     <div className="flex flex-col gap-2 md:hidden ">
       <div className="grid grid-cols-5 gap-2">
         {mobileFilterTitle.map((item) => (
-          <div key={item.key} className="flex items-center gap-2">
+          <div key={item.key} className="flex text-xs sm:text-sm items-center gap-2">
             {item.title}
             <SortBtn
               isActive={activeSorter?.field === item.key}
@@ -36,34 +36,38 @@ const ArtworkCardList = ({ artworks }: Props) => {
           className="bg-default-200 w-full rounded-lg cursor-pointer hover:bg-default-100"
         >
           <div className="flex gap-2 p-4">
-            <Image
-              src={`/images/${artwork.main_photo.transformations[1].filename_disk}`}
-              alt={artwork.artwork_name}
-              width={200}
-              height={200}
-              className="rounded-md object-cover object-center"
-            />
-            <div className="flex flex-col gap-1 w-full">
-              <div className="flex items-center justify-end gap-2 ">
+       <div  className="flex-1 sm:flex-none">
+           <Image
+               src={`/images/${artwork.main_photo.transformations[1].filename_disk}`}
+               alt={artwork.artwork_name}
+               width={150}
+               height={150}
+               className="rounded-md object-cover object-center"
+           />
+       </div>
+            <div className="flex flex-col gap-1 w-2/3 sm:w-full">
+              <div className="flex flex-wrap items-center justify-end gap-2 ">
                 <StatusBadge status={artwork.status} />
                 <VisibilityBadge visible={artwork.visibility} />
                 <DocsBadge docs={artwork.documents_number} />
               </div>
-              <div className="text-sm">
-                <div className="flex items-center gap-2">
+              <div className="text-xs sm:text-sm">
+                <div className="flex gap-2">
                   <b>Artwork:</b>
-                  <span className="truncate">{artwork.artwork_name}</span>
-                  {artwork.has_notifications && (
-                    <div className="w-[8px] h-[8px] bg-indigo-400 rounded-lg"></div>
-                  )}
+                <div className="flex items-center gap-1 truncate">
+                    <div className="truncate">{artwork.artwork_name}</div>
+                    {artwork.has_notifications && (
+                        <div className="w-[6px] h-[6px] bg-indigo-400 rounded-lg"></div>
+                    )}
                 </div>
-                <div className="flex items-center gap-2">
+                </div>
+                <div className="flex items-center gap-2 truncate">
                   <b>Author:</b> {artwork.artist_name}
                 </div>
                 <div className="flex items-center gap-2">
                   <b>Years:</b> {artwork.creation_year}
                 </div>
-                <div className="flex items-center gap-2 mb-2">
+                <div className="flex items-center  gap-2 mb-2">
                   <b>Price:</b> <span>{formatSum(artwork.price)}</span>
                   <Image
                     src="/icons/usdc.svg"
@@ -76,10 +80,10 @@ const ArtworkCardList = ({ artworks }: Props) => {
               </div>
               <div className="flex items-center justify-end gap-2">
                 <div className="flex items-center gap-2">
-                  <div className="text-sm">
+                  <div className="text-xs sm:text-sm">
                     <b>Views:</b> {formatSum(artwork.views)}
                   </div>
-                  <div className="text-sm">
+                  <div className="text-xs sm:text-sm">
                     <b>Updated:</b>{" "}
                     {artwork.date_updated
                       ? formatDate(artwork.date_updated)
--- components/SortBtn.tsx
+++ components/SortBtn.tsx
@@ -9,7 +9,7 @@ const SortBtn = ({ isActive, onClick }: Props) => {
       onClick={onClick}
       className="flex flex-col justify-center items-center cursor-pointer mt-1"
     >
-      <div className="flex flex-col gap-[3px] w-[12px] h-[12px]">
+      <div className="flex flex-col gap-[2px] w-[10px] h-[10px] md:w-[12px] md:h-[12px]">
         <span
           className={`h-[1px] bg-default-600 transition-all duration-300 ease-in-out ${isActive ? "w-full" : "w-[50%]"}`}
         ></span>
